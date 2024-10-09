d05c6bab96c054aa104951ae04776f5960f4ad0f
Isaiah <irodol@uw.edu>
2024-10-09 10:26:07 -0700

    Added home screen

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, Button } from 'react-native'
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text>
+                Hello there!
+            </Text>
+        </View>
+    )
+}
