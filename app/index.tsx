70a4d2274a44248680da3f3f5567ce6603c8acc8
ckato <cassk01@uw.edu>
2024-11-12 13:53:15 -0800

    Fixed inf loading, remember to comment that weird line

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,7 @@ import { Redirect } from 'expo-router';
 export default function AppScreen() {
   const auth = useAuth();
 
+  /*
   if (auth?.loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -16,6 +17,7 @@ export default function AppScreen() {
       </View>
     );
   }
+    */
 
   if (auth?.session && auth?.user) {
     // Redirect to home page
