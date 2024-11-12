79387914a0328c93aed8327b6f469a910d363862
ckato <cassk01@uw.edu>
2024-11-12 15:57:01 -0800

    Stream and Supabase seem to be connected properly

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import { router } from "expo-router";
 import { ChannelList } from "stream-chat-expo";
 import { Text } from "react-native-svg";
+import { useAuth } from '../../../providers/AuthProvider';
 
 export default function ChatScreen() {
+  const profile  = useAuth()?.profile;
   console.log('in tabs');
-  return <Text>Something</Text>;
-  // return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
+  // return <Text>Something</Text>;
+   return <ChannelList 
+      filters={{ members: { $in: [profile.id ]}}} 
+      onSelect={(channel) => router.push(`/channel/${channel.cid}`)} 
+   />;
 }
