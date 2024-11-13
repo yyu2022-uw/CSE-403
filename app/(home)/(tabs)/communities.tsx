c1d8c749f2ed771771ec20b2996021e4ea96882c
Isaiah <irodol@uw.edu>
2024-11-12 21:45:24 -0800

    Added communities drawer

--- app/(home)/(tabs)/communities.tsx
+++
@@ -1,7 +0,0 @@
-import { useAuth } from "../../../providers/AuthProvider";
-import { Text } from "react-native";
-
-export default function CommunitiesScreen() {
-    const auth = useAuth();
-    return <Text>{auth?.session?.user.id}</Text>
-}
