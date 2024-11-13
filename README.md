794a559bb86588a493a15792990a741a7387b5ab
Isaiah <irodol@uw.edu>
2024-11-12 22:47:40 -0800

    Wrote build and run instructions in README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Welcome to your Expo app 👋
+# Welcome to InterConnect 👋
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
 
@@ -13,7 +13,13 @@ This is an [Expo](https://expo.dev) project created with [`create-expo-app`](htt
 2. Start the app
 
    ```bash
-    npx expo start
+    npm start
+   ```
+
+3. Test the app
+
+   ```bash
+    npm test
    ```
 
 In the output, you'll find options to open the app in a
@@ -33,20 +39,10 @@ When you're ready, run:
 npm run reset-project
 ```
 
-This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
+## Project Specifics
 
-## Learn more
+To log out a user,
 
-To learn more about developing your project with Expo, look at the following resources:
-
-- [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
-- [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
-
-## Join the community
-
-Join our community of developers creating universal apps.
-
-- [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
-- [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
-
-t
+1. Uncomment the line in ```providers/AuthProvider.tsx```
+2. Type ```r``` in the terminal to reset the app
+3. Comment out the same line
--- providers/ChatProvider.tsx
+++ providers/ChatProvider.tsx
@@ -8,7 +8,6 @@
 // const client = StreamChat.getInstance('cvvs6gq2ynqy');
 // // const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY!);
 
-<<<<<<< HEAD
 // export default function ChatProvider({ children }: PropsWithChildren) {
 //   const [isReady, setIsReady] = useState(false);
 //   const profile = useAuth()?.profile;
@@ -63,59 +62,3 @@
 
 //   )
 // }
-=======
-export default function ChatProvider({ children }: PropsWithChildren) {
-  const [isReady, setIsReady] = useState(false);
-  const profile = useAuth()?.profile;
-
-  useEffect(() => {
-    console.log(profile);
-    if (!profile) {
-      return;
-    }
-
-    console.log("From chatProvider: " + profile.id);
-
-    const connect = async () => {
-      try {
-        await client.connectUser(
-          {
-            id: profile.id,
-            name: profile.full_name,
-            image: 'https://i.imgur.com/fR9Jz14.png',
-          },
-          client.devToken(profile.id),
-        );
-        setIsReady(true);
-      } catch (err) {
-        console.log(profile);
-        console.error(err);
-      }
-    };
-
-    connect();
-
-    return () => {
-      if (isReady) {
-        client.disconnectUser();
-      }
-      setIsReady(false);
-    }
-  }, [profile?.id])
-
-  if (!isReady) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
-
-  return (
-    <OverlayProvider>
-      <Chat client={client}>{children}</Chat>
-    </OverlayProvider>
-
-  )
-}
->>>>>>> communities
