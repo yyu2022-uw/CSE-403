0aedeeaf4e0394a0b0beee7b43b6de86f98d06b2
yyu2022 <yyu2022@cs.washington.edu>
2024-12-04 20:41:12 -0800

    weekly report

--- app/(home)/(tabs)/communities/communityPage.jsx
+++ app/(home)/(tabs)/communities/communityPage.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { RouteProp } from '@react-navigation/native';
-import { DrawerNavigationProp } from '@react-navigation/drawer';
 
 function CommunityPage({ route }) {
     const { name } = route.params; // Access the name passed via initialParams
